c68aa5e0b1a3cfdade1cd0e75a29883cc6a64ec3
61653 <61653@0338S-S-R0B6ZGX.sdcs.local>
2019-09-24 14:24:17 -0700

    houses

--- src/_01_houses/houses.java
+++ src/_01_houses/houses.java
@@ -5,10 +5,21 @@ import org.jointheleague.graphical.robot.Robot;
 public class houses {
 public static void main(String[] args) {
 	Robot rob = new Robot();
-	rob.moveTo(50, 500);
+	rob.penDown();
+	rob.miniaturize();
+	rob.setSpeed(25);
+	rob.moveTo(10, 550);
 	rob.move(100);
 	rob.turn(90);
-	rob.move(20);
+	rob.move(30);
+	rob.turn(90);
+	rob.move(100); 
+	rob.turn(-90);
+	rob.setPenColor(0, 150, 0);
+	rob.move(35);
+
 	
 }
 }
+
+ 
